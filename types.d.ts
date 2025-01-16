0f483f1cb34647c7cbd9b662597247c086518e04
Tridominous <jamesmou02@gmail.com>
2025-01-16 13:21:40 +0000

    Add Hero section, BookCover, BookOverview, BookCoverSvg components

---
+++ types.d.ts
@@ -0,0 +1,14 @@
+interface Book {
+    title: string;
+    author: string;
+    genre: string; 
+    rating: number; 
+    totalCopies: number;
+    availableCopies: number;
+    coverColor: string;
+    coverUrl: string;
+    description: string;
+    video: string;
+    summary: string;
+    isLoanedBook: boolean;
+}

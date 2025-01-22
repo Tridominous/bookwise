beb0663642a8c0c4cb2bb3f1c6659b6d1c1b0da3
Tridominous <jamesmou02@gmail.com>
2025-01-22 15:15:48 +0000

    work on oboarding email sending logic with Resend

--- lib/workflow.ts
+++ lib/workflow.ts
@@ -1,8 +1,27 @@
 import config from "./config";
 import { Client as WorkflowClient } from "@upstash/workflow";
+import { Client as QstashClient, resend } from "@upstash/qstash";
 
 
 export const workflowClient = new WorkflowClient({
     baseUrl: config.env.upstash.qstashUrl,
     token: config.env.upstash.qstashToken,
-})
+});
+
+const qstashClient = new QstashClient({ token: config.env.upstash.qstashToken });
+
+export const sendEmail = async ({email, subject, message}: {email: string, subject: string, message: string}) => {
+    await qstashClient.publishJSON({
+      api: {
+        name: "email",
+        provider: resend({ token: config.env.resendToken }),
+      },
+      body: {
+        from: "James Mou <hello.jamesmou.com>",
+        to: [email],
+        subject: subject,
+        html: message,
+      },
+    });
+
+}
